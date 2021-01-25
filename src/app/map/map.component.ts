6c47dfcd61c45d49f0034cc3023c074faa0c4762
marco <marco.a.strauss@gmx.de>
2021-01-25 08:22:47 +0100

    no message

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -40,7 +40,8 @@ export class MapComponent implements AfterViewInit {
 
   private initMap(): void{
     proj4.defs("EPSG:3857","+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs");
-    register(proj4)
+    register(proj4);
+    console.log("loading");
     this.projection = GetProjection('EPSG:3857');
     this.projection.setExtent(this.extent);
     this.view = new View({
